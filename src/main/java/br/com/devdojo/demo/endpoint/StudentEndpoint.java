72a7318cd4222144bf15af8db7a69adae154cde6
mauriciopedroso26 <mauricio.pedroso@southsystem.com.br>
2018-12-09 17:36:59 -0200

    Aula 11

--- src/main/java/br/com/devdojo/demo/endpoint/StudentEndpoint.java
+++ src/main/java/br/com/devdojo/demo/endpoint/StudentEndpoint.java
@@ -1,12 +1,15 @@
 package br.com.devdojo.demo.endpoint;
 
 import br.com.devdojo.demo.error.CustomErrorType;
+import br.com.devdojo.demo.error.ResourceNotFoundException;
 import br.com.devdojo.demo.model.Student;
 import br.com.devdojo.demo.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 
 @RestController
@@ -27,16 +30,14 @@ public class StudentEndpoint {
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getStudentById(@PathVariable("id") Long id) {
-        Student student = studentRepository.findById(id).get();
-        if (student == null) {
-            return new ResponseEntity<>(new CustomErrorType("Student not found"), HttpStatus.NOT_FOUND);
-        }
+        verifyIfStudentsExistis(id);
 
+        Optional<Student> student = studentRepository.findById(id);
         return new ResponseEntity<>(student, HttpStatus.OK);
     }
 
     @GetMapping("/findByName/{name}")
-    public ResponseEntity<?> findStudentByName(@PathVariable("name") String name){
+    public ResponseEntity<?> findStudentByName(@PathVariable("name") String name) {
         return new ResponseEntity<>(studentRepository.findByNameIgnoreCaseContaining(name), HttpStatus.OK);
     }
 
@@ -47,13 +48,25 @@ public class StudentEndpoint {
 
     @DeleteMapping("{id}")
     public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+        verifyIfStudentsExistis(id);
+
         studentRepository.deleteById(id);
-        return new  ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PutMapping
     public ResponseEntity<?> update(@RequestBody Student student) {
+        verifyIfStudentsExistis(student.getId());
+
         studentRepository.save(student);
         return new ResponseEntity<>(student, HttpStatus.OK);
+    }
+
+    private void verifyIfStudentsExistis(Long id){
+        Optional<Student> student = studentRepository.findById(id);
+        if (!student.isPresent()) {
+            throw new ResourceNotFoundException("Student not found for ID: " + id);
+        }
+
     }
 }
