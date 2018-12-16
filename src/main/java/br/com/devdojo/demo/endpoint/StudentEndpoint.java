674e55d3b7c41246dd954858b91defbf53555acd
mauriciopedroso26 <mauricio.pedroso@southsystem.com.br>
2018-12-16 20:12:26 -0200

    Aula 13

--- src/main/java/br/com/devdojo/demo/endpoint/StudentEndpoint.java
+++ src/main/java/br/com/devdojo/demo/endpoint/StudentEndpoint.java
@@ -6,6 +6,7 @@ import br.com.devdojo.demo.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -41,6 +42,7 @@ public class StudentEndpoint {
     }
 
     @PostMapping
+    @Transactional
     public ResponseEntity<?> save(@RequestBody Student student) {
         return new ResponseEntity<>(studentRepository.save(student), HttpStatus.OK);
     }
