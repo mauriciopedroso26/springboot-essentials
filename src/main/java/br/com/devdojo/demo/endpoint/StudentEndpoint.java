41fedc6cc36968c8459b8fcc7a5411ddc8f7687e
mauriciopedroso26 <mauricio.pedroso@southsystem.com.br>
2018-12-31 16:25:33 -0200

    Aula 18

--- src/main/java/br/com/devdojo/demo/endpoint/StudentEndpoint.java
+++ src/main/java/br/com/devdojo/demo/endpoint/StudentEndpoint.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -37,6 +40,7 @@ public class StudentEndpoint {
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getStudentById(@PathVariable("id") Long id) {
+
         verifyIfStudentsExistis(id);
 
         Optional<Student> student = studentRepository.findById(id);
@@ -55,6 +59,7 @@ public class StudentEndpoint {
     }
 
     @DeleteMapping("{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> delete(@PathVariable("id") Long id) {
         verifyIfStudentsExistis(id);
 
